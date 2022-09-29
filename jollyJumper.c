0442daa2cd1718e41890b415d085fc9c3f24751d
LCTheLegend <70448473+LCTheLegend@users.noreply.github.com>
2022-09-29 14:10:57 +0200

    Update jollyJumper.c

--- jollyJumper.c
+++ jollyJumper.c
@@ -4,8 +4,25 @@
 
 
 
-int isJollyJumber(const int seq[], int size) {
-/*post: answer to exercise 7.a*/
-    return 1;
-}
+int isJollyJumber(const int seq[], int size) 
+{
+    bool diffs_found[size - 1]; //Bool array to check if lengths up to size-1 has been found
 
+    for(int i = 1 ; i < size ; i++)
+    {
+        int diff;
+
+        diff = abs(seq[i] - seq[i - 1]);
+        diffs_found[diff] = true;
+    } //loop finds all the differences and sets value to true in diffs_found if its found
+
+    for(int i = 1 ; i < size ; i++)
+    {
+        if(diffs_found[i] != true)
+        {
+            printf("Sequence is not Jolly");
+            return 0;
+        }   
+    } //This section checks if all differences up to size-1 has been found, if not the loop ends prematurely, if true the line below is printed
+    printf("Sequence is Jolly");
+}
